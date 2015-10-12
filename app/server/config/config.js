82f3ac69367afcec09e2b40b30b7eb8e470f557b
Jordane Giuly <jordane.giuly@gmail.com>
2015-10-12 20:56:36 +0200

    add board and list id

--- app/server/config/config.js
+++ app/server/config/config.js
@@ -1,11 +1,23 @@
 var url = 'http://gitello.coorpacademy.com';
+var board = {
+	boardId: '561228dc16f33267799133c3',
+	lists: {
+		inProgress: '561244c5945acda7e5626f90'
+	}
+};
 
 if (process.env.NODE_ENV === 'development') {
 	var auth = require('../../../.auth.dev.json');
 	process.env.GITHUB_ACCESS_TOKEN = auth.GITHUB_ACCESS_TOKEN;
 	process.env.TRELLO_PUBLIC = auth.TRELLO_PUBLIC;
 	process.env.TRELLO_TOKEN = auth.TRELLO_TOKEN;
-	url = 'http://13258555.ngrok.io'
+	url = 'http://13258555.ngrok.io';
+	board = {
+		boardId: '55fa8a07f150fe773e901c4d',
+		lists: {
+			inProgress: '560a581ae66d38d7816086bc'
+		}
+	}
 }
 
 module.exports = {
@@ -20,7 +32,12 @@ module.exports = {
 	trello: {
 		publicKey: process.env.TRELLO_PUBLIC || '',
 		accessToken: process.env.TRELLO_TOKEN || '',
-		boardId: '55fa8a07f150fe773e901c4d',
+		board: {
+			boardId: '55fa8a07f150fe773e901c4d',
+			lists: {
+				inProgress: '560a581ae66d38d7816086bc'
+			}
+		},
 		callbackUrl: '/webhooks/trello'
 	}
 }
