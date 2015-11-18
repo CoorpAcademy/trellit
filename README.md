8afbbe83792ce92cbf9c0d83b724410403b230b2
Jordane Giuly <jordane.giuly@gmail.com>
2015-11-18 17:51:17 +0100

    split config and edit readme

--- README.md
+++ README.md
@@ -1,13 +1,60 @@
 # trellit
 Synchronization app between Trello and Github
 
-
-
 ## Install the project
 
-- npm install
+`npm install`
+
+## Install NGROK
+
+We need a software like ngrok to expose our localhost on the web. To download it, go here: https://ngrok.com/
+Then, to run it, type:
+
+`./ngrok http PORT`
+
+A line like this one should be printed:
+
+`http://937aba9e.ngrok.io -> localhost:3000`
 
 ## Set configuration
 
-//https://trello.com/1/connect?key=KEY&name=trellit&expiration=never&response_type=token&scope=read,write
+### auth
+Copy paster the file "auth.json" and rename it by '.auth.json'
 
+- To get your TRELLO_PUBLIC: https://trello.com/app-key
+
+- To get your GITHUB_ACCESS_TOKEN: https://github.com/settings/tokens with repo rights
+
+- To get your TRELLO_TOKEN: https://trello.com/1/connect?key=TRELLO_PUBLIC&name=Trellit&response_type=token&expiration=never&scope=read,write
+
+### Config
+
+- In config.js, you need to edit the url variable. In the development part, enter the value returned by the ngrok script.
+
+- For repos and boards, you just need to edit the boards.ENV.json and repos.ENV.json files.
+
+
+### members
+Set the list of your Github and Trello members here. A member needs to have the following attributes:
+
+```
+   {
+      "github": {
+         "login": "xxx"
+      },
+      "trello": {  
+         "id":"xxx",
+         "username":"xxx",
+         "fullName":"xxx",
+         "bio":""
+      }
+   }
+```
+
+./ngrok VERSION http PORT
+
+
+
+## Run the project
+
+`NODE_ENV=development npm start`
--- app/server/config/config.js
+++ app/server/config/config.js
@@ -1,60 +1,17 @@
 var url = 'https://trellit.coorpacademy.com';
-var boards = {
-	backlog: '56128e84b038bd3747a4687d',
-	currentSprint: '561228dc16f33267799133c3'
-};
 
-var lists = {
-	backlog: '562616318bbc8958aa72a18c',
-	todo: '56128ed20f503e65784153ae',
-	inProgress: '561244c5945acda7e5626f90',
-	toReview: '561244d2b74a46b20115ea3c',
-	toTest: '561244d5d3830880a92e8c1e',
-	done: '561b9681095780ac16807534'
-};
-var repos = [
-	{
-		user: 'CoorpAcademy',
-		repo: 'coorpacademy'
-	},
-	{
-		user: 'CoorpAcademy',
-		repo: 'coorpacademy-store'
-	},
-	{
-		user: 'CoorpAcademy',
-		repo: 'coorpacademy-connect'
-	},
-	{
-		user: 'CoorpAcademy',
-		repo: 'coorpacademy-www-wp'
-	}
-];
+var boards = require('./boards.production.json');
+var repos = require('./repos.production.json');
 
 if (process.env.NODE_ENV === 'development') {
-	var auth = require('../../../.auth.dev.json');
+	var auth = require('../../../.auth.json');
 	process.env.GITHUB_ACCESS_TOKEN = auth.GITHUB_ACCESS_TOKEN;
 	process.env.TRELLO_PUBLIC = auth.TRELLO_PUBLIC;
 	process.env.TRELLO_TOKEN = auth.TRELLO_TOKEN;
 
-	url = 'http://aeb0bf00.ngrok.io';
-	boards = {
-		currentSprint: '55fa8a07f150fe773e901c4d'
-	};
-	lists = {
-		backlog: '5610f178001cf35b47192a86',
-		todo: '56058332b9be8215c8ebe4d3',
-		inProgress: '560a581ae66d38d7816086bc',
-		toReview: '55fa8b7006b70ca73d29512e',
-		toTest: '56027a7779381592829ffa89',
-		done: '5601f04b98142f9e3caa68f1'
-	};
-	repos = [
-		{
-			user: 'CoorpAcademy',
-			repo: 'trellit'
-		}
-	];
+	url = 'http://937aba9e.ngrok.io';
+	boards = require('./boards.development.json');
+	repos = require('./repos.development.json');
 }
 
 module.exports = {
@@ -68,8 +25,8 @@ module.exports = {
 	trello: {
 		publicKey: process.env.TRELLO_PUBLIC || '',
 		accessToken: process.env.TRELLO_TOKEN || '',
-		boards: boards,
-		lists: lists,
+		boards: boards.boards,
+		lists: boards.lists,
 		callbackUrl: '/webhooks/trello'
 	}
 }
